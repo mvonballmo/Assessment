a6f9608a6d3be2f2bf8476f430a6dd96e8a70df2
Marco Von Ballmoos <marco@earthli.com>
2022-01-17 10:01:44 +0100

    Update Bitcoin currency-retrieval code

--- index.php
+++ index.php
@@ -1,17 +1,6 @@
 <?php
 
-echo "jello";
+$url='https://api.coinbase.com/v2/prices/spot?currency=CHF';
+$bitcoinInCHF=json_decode(file_get_contents($url));
 
-$url='https://bitpay.com/api/rates';
-$json=json_decode( file_get_contents( $url ) );
-$dollar=$btc=0;
-
-foreach( $json as $obj ){
-  if( $obj->code=='USD' )$btc=$obj->rate;
-}
-
-echo "1 bitcoin=\$" . $btc . "USD<br />";
-$dollar=1 / $btc;
-echo "10 dollars = " . round( $dollar * 10,8 )."BTC";
-
-//https://api.coinbase.com/v2/prices/spot?currency=USD
+echo "1 bitcoin = CHF" . $bitcoinInCHF->data->amount;
